835ebd1dfb184616e9bd13db2843c2a9fde901d4
Vicnava95 <47409039+Vicnava95@users.noreply.github.com>
2021-01-13 11:07:31 -0600

    add formulario

---
+++ resources/views/form/form.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.security')
+@section('section')
+Formulario 
+@endsection
+
+@section('css_before')
+
+@endsection
+
+@section('css_after')
+
+@endsection
+
+@section('content')
+<div class="auth-wrapper">
+    <div class="auth-content">
+        <div class="auth-bg">
+            <span class="r"></span>
+            <span class="r s"></span>
+            <span class="r s"></span>
+            <span class="r"></span>
+        </div>
+        <div class="card">
+            <div class="card-body text-center">
+                <div class="mb-4">
+                    <i class="feather icon-user-plus auth-icon"></i>
+                </div>
+                <h3 class="mb-4"> <b>Ministerio Internacional Compaz</b></h3></br>
+                <h3 class="mb-4">SOLICITUD DE INGRESO AL MINISTERIO DE UJIERES</h3>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Username">
+                </div>
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control" placeholder="Email">
+                </div>
+                <div class="input-group mb-4">
+                    <input type="password" class="form-control" placeholder="password">
+                </div>
+                <div class="form-group text-left">
+                    <div class="checkbox checkbox-fill d-inline">
+                        <input type="checkbox" name="checkbox-fill-1" id="checkbox-fill-1" checked="">
+                        <label for="checkbox-fill-1" class="cr"> Save Details</label>
+                    </div>
+                </div>
+                <div class="form-group text-left">
+                    <div class="checkbox checkbox-fill d-inline">
+                        <input type="checkbox" name="checkbox-fill-2" id="checkbox-fill-2">
+                        <label for="checkbox-fill-2" class="cr">Send me the <a href="#!"> Newsletter</a> weekly.</label>
+                    </div>
+                </div>
+                <button class="btn btn-primary shadow-2 mb-4">Sign up</button>
+                <p class="mb-0 text-muted">Allready have an account? <a href="auth-signin.html"> Log in</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('js_after')
+@endsection
